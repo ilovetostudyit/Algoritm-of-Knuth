51e68491f4f544f15f8eda0746b4275989c617b4
Ezzelyno Haggon <ehaggon@e1r11p3.21-school.ru>
2019-01-29 17:03:51 +0300

    i start to understand

--- FILLIT/min_square.c
+++ FILLIT/min_square.c
@@ -1,16 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   form_validation.c                                  :+:      :+:    :+:   */
+/*   min_square.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehaggon <ehaggon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/23 16:13:26 by ehaggon           #+#    #+#             */
-/*   Updated: 2019/01/23 16:14:29 by ehaggon          ###   ########.fr       */
+/*   Created: 2019/01/29 14:44:42 by ehaggon           #+#    #+#             */
+/*   Updated: 2019/01/29 15:49:32 by ehaggon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int form_validation(char [4][4])
+static int ft_sqrmin(int sq)
 {
+	int min;
 
+	min = 0;
+	while((min * min) < sq)
+	{
+		min++;
+	}
+	return(min);
 }
+
+int min_square(char vd[8][8])
+{
+	int sq;
+	int x;
+	int y;
+
+	sq = 0;
+	x = 0;
+	y = 0;
+	while (x < 8)
+	{
+		while (y < 4)
+		{
+			if (vd[x][y] == '1')
+				sq++;
+
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	sq = ft_sqrmin(sq);
+	return(sq);
+}
+
+
